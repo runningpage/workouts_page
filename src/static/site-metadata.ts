5e667c206e19033681a6c801e9e29ed132c9c342
cuijianzhuang <cui2421011864@gmail.com>
2025-01-17 09:28:52 +0800

    更新个人信息

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,18 +12,18 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
+  siteUrl: 'https://cuijianzhuang.com',
   logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://ben29.xyz',
+      name: 'HomePage',
+      url: 'https://cuijianzhuang.com',
     },
     {
       name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      url: 'https://github.com/cuijianzhuang',
     },
   ],
 };
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoiYmVuLTI5IiwiYSI6ImNrZ3Q4Ym9mMDBqMGYyeXFvODV2dWl6YzQifQ.gSKoWF-fMjhzU67TuDezJQ';
+  'pk.eyJ1IjoiY3VpcGx1cyIsImEiOiJjbHYxMnA0ZTAwMm5lMm5wYzJjeXJwOGQyIn0.tdHoQD-xn2jC_pY1R_1Sag';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
